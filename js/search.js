16346eab7a6f9b538f89e37ad6e9f75be9290036
melissamyra <35929806+melissamyra@users.noreply.github.com>
2020-04-27 02:19:45 +0800

    Updated input value for case insensitivity

--- js/search.js
+++ js/search.js
@@ -2,11 +2,11 @@ $(document).ready( function(){
   //***** focus in *****
   $('#search').on('keyup focus', () => {
     // $('.gallery').hide();
-    let search = $('#search').val();
+    let search = $('#search').val().toUpperCase();
 
     //***** loop through all a tag with lightGallery attributes *****
     $('.gallery').each( (i, img) => {
-      let string = $('.gallery')[i].getAttribute('data-sub-html').toLowerCase();
+      let string = $('.gallery')[i].getAttribute('data-sub-html').toUpperCase();
       // console.log(string); //test
       if (string.indexOf(search) > -1){
   //     images[i].addClassName('hide'); //give class name with hidden properties in CSS
