5f8657a0b02775e9f3b60f2b4b157fa076d1e867
melissamyra <35929806+melissamyra@users.noreply.github.com>
2020-04-27 02:51:49 +0800

    Cleared commented code

--- js/search.js
+++ js/search.js
@@ -1,15 +1,15 @@
 $(document).ready( function(){
   //***** focus in *****
   $('#search').on('keyup focus', () => {
-    // $('.gallery').hide();
+
     let search = $('#search').val().toUpperCase();
 
     //***** loop through all a tag with lightGallery attributes *****
     $('.gallery').each( (i, img) => {
       let string = $('.gallery')[i].getAttribute('data-sub-html').toUpperCase();
-      // console.log(string); //test
+
       if (string.indexOf(search) > -1){
-  //     images[i].addClassName('hide'); //give class name with hidden properties in CSS
+
         $('.gallery')[i].style.display = "block";
       } else {
         $('.gallery')[i].style.display = "none";
@@ -20,6 +20,4 @@ $(document).ready( function(){
   // $('#search').on(' blur focusout', () => {
   //   $('.gallery').show();
   // });
-
-
 });
