4ccbcaa938f74389aa04db391baec3b97ca3f791
melissamyra <35929806+melissamyra@users.noreply.github.com>
2020-04-27 00:29:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# techdegree-project-5
+ Treehouse FEWD Techdegree Project 5 - JavaScript and jQuery
---
+++ js/search.js
@@ -0,0 +1,25 @@
+$(document).ready( function(){
+  //***** focus in *****
+  $('#search').on('keyup focus', () => {
+    // $('.gallery').hide();
+    let search = $('#search').val();
+
+    //***** loop through all a tag with lightGallery attributes *****
+    $('.gallery').each( (i, img) => {
+      let string = $('.gallery')[i].getAttribute('data-sub-html').toLowerCase();
+      // console.log(string); //test
+      if (string.indexOf(search) > -1){
+  //     images[i].addClassName('hide'); //give class name with hidden properties in CSS
+        $('.gallery')[i].style.display = "block";
+      } else {
+        $('.gallery')[i].style.display = "none";
+      } //***** OMG IT WORKED! *****
+    });
+  });
+  // *****focus out *****
+  // $('#search').on(' blur focusout', () => {
+  //   $('.gallery').show();
+  // });
+
+
+});
