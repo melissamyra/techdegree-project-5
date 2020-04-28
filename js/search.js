5018ba0272e6038a2b7457b6ceae1b681cc5555e
melissamyra <35929806+melissamyra@users.noreply.github.com>
2020-04-29 04:20:42 +0800

    Added comments

--- js/search.js
+++ js/search.js
@@ -1,23 +1,19 @@
 $(document).ready( function(){
-  //***** focus in *****
+  //***** listen on input focus / user input *****
   $('#search').on('keyup focus', () => {
-
+    //save user input
     let search = $('#search').val().toUpperCase();
-
     //***** loop through all a tag with lightGallery attributes *****
     $('.gallery').each( (i, img) => {
+      //save captions
       let string = $('.gallery')[i].getAttribute('data-sub-html').toUpperCase();
-
+      //if caption contains words from user input
       if (string.indexOf(search) > -1){
-
+        //show image
         $('.gallery')[i].style.display = "block";
-      } else {
+      } else { //else hide image
         $('.gallery')[i].style.display = "none";
       } //***** OMG IT WORKED! *****
     });
   });
-  // *****focus out *****
-  // $('#search').on(' blur focusout', () => {
-  //   $('.gallery').show();
-  // });
 });
